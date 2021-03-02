b12481dc65849df2c73bb300b9d20001c226c24f
Anthony <aalbane6@binghamton.edu>
2021-03-02 00:00:18 -0500

    FInal

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # exercise-late
+I couldn't find a repo so I made my own.
+I dont know if this counts for credit anymore but I attempted it anyway.
+I had to drive home after being released.
---
+++ main.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main()
+{
+	char str[] = "C Programming is Great!";
+	
+	printf("%s\n", str);
+	printf("%d\n", sizeof(str));
+	printf("%d\n", str[0]);
+	printf("%d\n", str[2]);
+	printf("%d\n", str[3]);
+	printf("%d\n", str[4]);
+	printf("%d\n", str[5]);
+	printf("%d\n", str[6]);
+	printf("%d\n", str[7]);
+	printf("%d\n", str[8]);
+	printf("%d\n", str[9]);
+	printf("%d\n", str[10]);
+	printf("%d\n", str[11]);
+	printf("%d\n", str[12]);
+	printf("%d\n", str[14]);
+	printf("%d\n", str[15]);
+	printf("%d\n", str[17]);
+	printf("%d\n", str[18]);
+	printf("%d\n", str[19]);
+	printf("%d\n", str[20]);
+	printf("%d\n", str[21]);
+	printf("%d\n", str[22]);
+	printf("%c\n", str[0] = 'c');
+	printf("%c\n", str[2] = 'p');
+	printf("%c\n", str[3] = 'R');
+	printf("%c\n", str[4] = 'O');
+	printf("%c\n", str[5] = 'G');
+	printf("%c\n", str[6] = 'R');
+	printf("%c\n", str[7] = 'A');
+	printf("%c\n", str[8] = 'M');
+	printf("%c\n", str[9] = 'M');
+	printf("%c\n", str[10] = 'I');
+	printf("%c\n", str[11] = 'N');
+	printf("%c\n", str[12] = 'G');
+	printf("%c\n", str[14] = 'I');
+	printf("%c\n", str[15] = 'S');
+	printf("%c\n", str[17] = 'g');
+	printf("%c\n", str[18] = 'R');
+	printf("%c\n", str[19] = 'E');
+	printf("%c\n", str[20] = 'A');
+	printf("%c\n", str[21] = 'T');
+	
+}
